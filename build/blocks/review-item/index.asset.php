dd3d76dad4dfa8226bba94758e6a465ceb7ff81b
nazmul-huda <thankyounayem2@gmail.com>
2023-05-02 11:16:06 +0600

    changes

--- build/blocks/review-item/index.asset.php
+++ build/blocks/review-item/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'cfa87d784285c5792898');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '52c52ba1c76986f6eaa7');
